36e579785a6eef6618233d251695ca864f6c4f65
LuRhino <chrislu30604@gmail.com>
2018-12-30 01:17:33 +0800

    add multi

--- single.py
+++ single.py
@@ -7,6 +7,7 @@ from download import setup_download_dir, get_links, download_link
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 logger = logging.getLogger(__name__)
 
+
 def main():
     ts = time()
     client_id = os.getenv('IMGUR_CLIENT_ID')
